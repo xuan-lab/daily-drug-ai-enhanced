30a1a711cca945699571956e9bb0d3f7a679e2b3
xuan-lab <xuan1024chem@gmail.com>
2025-04-22 20:41:45 +0800

    commit

--- fetch_summarize_papers.py
+++ fetch_summarize_papers.py
@@ -120,7 +120,7 @@ def summarize_text_deepseek(text, api_key):
         "Authorization": f"Bearer {api_key}"
     }
     prompt = (
-        "请提供以下学术论文摘要的简明中文总结（约3-5句话），"
+        "请提供以下学术论文摘要的简明中文总结（约4-6句话），"
         "重点关注其关键发现及其与药学或药物科学的相关性,请额外列出创新性总结和药学启示\n\n"
         f"{text}"
     )
